ef892b9f3058373774d173c4a297d8dd22fd8d00
daichi-yoshikawa <daichi.yoshikawa@gmail.com>
2025-02-21 23:18:38 -0800

    feat: Created waveshare epd instance factory.

--- dexcreen/screen.py
+++ dexcreen/screen.py
@@ -4,15 +4,30 @@ import importlib
 from dotenv import load_dotenv
 
 
-class WaveshareEpd:
-  @staticmethod
-  def get_instance():
-    load_dotenv()
-    screen_module = os.getenv('SCREEN_MODULE', 'epd7in5_V2')
+load_dotenv()
 
+class WaveshareEpd:
+  screen_module = None
+
+  @classmethod
+  def import_screen_module(cls):
+    screen_module = os.getenv('SCREEN_MODULE', 'epd7in5_V2')
     try:
-      pymodule = importlib.import_module(f'waveshare_epd.{screen_module}')
+      cls.screen_module = importlib.import_module(f'waveshare_epd.{screen_module}')
     except ModuleNotFoundError:
       raise ImportError(f'Could not import waveshare_epd.{screen_module}')
 
-    return pymodule.EPD()
+  @classmethod
+  def get_instance(cls):
+    if cls.screen_module is None:
+      cls.import_screen_module()
+    return cls.screen_module.EPD()
+
+  @classmethod
+  def module_exit(cls):
+    if cls.screen_module is None:
+      cls.import_screen_module()
+    cls.screen_module.epdconfig.module_exit(cleanup=True)
+    
+
+    
